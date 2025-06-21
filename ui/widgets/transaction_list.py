10d5bacb4ecf0bac6a4a05bae84a22e464433c4c
joananjohn <joananjohnjacob9@gmail.com>
2025-06-21 18:05:13 +0200

    Added Delete Button

--- ui/widgets/transaction_list.py
+++ ui/widgets/transaction_list.py
@@ -1,8 +1,8 @@
-from PySide6.QtWidgets import QWidget, QTableWidget, QVBoxLayout,QSizePolicy,QHeaderView,QTableWidgetItem
+from PySide6.QtWidgets import QWidget, QTableWidget, QVBoxLayout,QSizePolicy,QHeaderView,QTableWidgetItem,QPushButton
 from models.transaction import Transaction
 from controllers.transaction_controller import TransactionManager
 
-LABELS = ["Date", "Id", "Amount","Category","Description"]
+LABELS = ["Date", "Id", "Amount","Category","Description","Delete"]
 
 class TransactionTable(QWidget):
     def __init__(self,controller,parent=None):
@@ -15,7 +15,7 @@ class TransactionTable(QWidget):
     def setup_ui(self):
         layout = QVBoxLayout()
         self.transaction_table = QTableWidget()
-        self.transaction_table.setColumnCount(5) # For Date, id, amount, category,description 
+        self.transaction_table.setColumnCount(6) # For Date, id, amount, category,description, Delete
         self.transaction_table.setHorizontalHeaderLabels(LABELS)
 
         layout.addWidget(self.transaction_table)
@@ -28,18 +28,27 @@ class TransactionTable(QWidget):
     def load_transaction(self):
         #clear the table
         self.transaction_table.setRowCount(0)
+        
+
 
         #Get all transaction
         transactions = self.controller.get_all_transactions()
         
         #Looping through transaction
         for index,transaction in enumerate(transactions):
+            delete_button = QPushButton("Delete")
+            delete_button.clicked.connect(lambda checked,tx_id = transaction.id:self.delete_transaction(tx_id))
             self.transaction_table.insertRow(index)
             self.transaction_table.setItem(index,0,QTableWidgetItem(transaction.date))
             self.transaction_table.setItem(index,1,QTableWidgetItem(transaction.id))
             self.transaction_table.setItem(index,2,QTableWidgetItem(str(transaction.amount)))
             self.transaction_table.setItem(index,3,QTableWidgetItem(transaction.category))
             self.transaction_table.setItem(index,4,QTableWidgetItem(transaction.description))
+            self.transaction_table.setCellWidget(index,5,delete_button)
+    
+    def delete_transaction(self,txn_id):
+        self.controller.delete_transaction(txn_id)
+        self.load_transaction()
 
                                            
 
